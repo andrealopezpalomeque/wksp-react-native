d6ec913e1ef38f7adeb3e976d375d14f51edafa7
andrealopezpalomeque <andrealopezpalomeque@gmail.com>
2022-12-27 14:59:55 -0300

    trying first commit

--- NestingNavigators/App.js
+++ NestingNavigators/App.js
@@ -1,22 +1,20 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { Button, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { Button, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 const About = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>About</Text>
       <Button
         title="Settings"
-        onPress={() =>
-          navigation.navigate('Settings')
-        }
+        onPress={() => navigation.navigate("Settings")}
       />
     </View>
   );
@@ -24,11 +22,11 @@ const About = ({ navigation }) => {
 
 const List = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>List</Text>
     </View>
   );
-}
+};
 
 const Home = () => {
   return (
@@ -37,29 +35,30 @@ const Home = () => {
       <Tab.Screen name="About" component={About} />
     </Tab.Navigator>
   );
-}
+};
 
 const Settings = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Settings</Text>
     </View>
+    //comment
   );
-}
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        // screenOptions={{
-        //   headerShown: false
-        // }}
+      // screenOptions={{
+      //   headerShown: false
+      // }}
       >
-        <Stack.Screen 
-          name="Home" 
-          component={Home} 
+        <Stack.Screen
+          name="Home"
+          component={Home}
           options={{
-            headerShown: false
+            headerShown: false,
           }}
         />
         <Stack.Screen name="Settings" component={Settings} />
